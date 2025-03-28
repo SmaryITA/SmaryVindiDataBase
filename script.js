4db9563d36a2698fab8d911c730331876c8444f4
SmaryITA <marialibera95@libero.it>
2025-03-28 23:23:14 +0100

    added new scrolls sections + buttons + missing icons  + updates css-js

--- script.js
+++ script.js
@@ -44,3 +44,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var acc = document.getElementsByClassName("accordion-scroll");
+  var panels = document.getElementsByClassName("panel-scroll");
+
+  for (var j = 0; j < panels.length; j++) {
+    panels[j].style.display = "none";
+  }
+
+  for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+});
